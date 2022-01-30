ccfdcc6cb7a91c97ea5e1a8e4bd6081415326420
shormiee <76555675+shormiee@users.noreply.github.com>
2022-01-30 23:05:56 +0600

    Update main.dart

--- main.dart
+++ main.dart
@@ -2,46 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chatapp/screens/auth-screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-//
-// Future<void> main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp();
-//   runApp(MyApp());
-// }
-//
-// class MyApp extends StatelessWidget {
-//
-//   @override
-//
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-//         backgroundColor: Colors.blueAccent,
-//         accentColor: Colors.deepPurpleAccent,
-//         accentColorBrightness: Brightness.dark,
-//         buttonTheme: ButtonTheme.of(context).copyWith(
-//           buttonColor: Colors.pinkAccent,
-//           shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20),
-//           ),
-//         ),
-//       ),
-//       home:
-
-      // StreamBuilder(stream:FirebaseAuth.instance.authStateChanges(),
-      //     builder: (ctx, userSnapshot) {
-      //     if (userSnapshot.hasData)
-      //    {
-      //         return ChatScreen();
-      //    }
-      //    return AuthScreen();
-      //     }
-      // ),
-//     );
-//   }
-// }
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_chatapp/screens/home.dart';
